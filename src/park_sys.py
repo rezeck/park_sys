84d9b5824fad5e19bd79749a538372b6caf89b1e
Rezeck <rezeck@dcc.ufmg.br>
2016-11-30 16:14:35 -0200

    Fix launch file

--- src/park_sys.py
+++ src/park_sys.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import rospy
 
 from geometry_msgs.msg import Twist
 
 def run():
+    rospy.init_node('park_sys', anonymous=False)
+
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('park_sys', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         rate.sleep()
